f45d6df35ee3c51f8eda95b0b5dfbe819f730456
Speed <Speed@srlab39>
2014-11-20 14:17:59 +0530

    Initial data staging

---
+++ JavaApplication4/src/emaildata.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Speed
+ */
+
+
+public class emaildata {
+String uid,upw,upv;
+public void storeemaildetails(){
+        String username=uid;
+        String password=upw;
+        String provider=upv;
+}
+}
